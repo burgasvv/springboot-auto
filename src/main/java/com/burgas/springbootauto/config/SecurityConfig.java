735c96557f6dabcf4fa64748c59e3214fbc37299
Burgas <burgassme@gmail.com>
2024-05-22 11:47:41 +0700

    Добавление Spring Security регистрации и авторизации

--- src/main/java/com/burgas/springbootauto/config/SecurityConfig.java
+++ src/main/java/com/burgas/springbootauto/config/SecurityConfig.java
@@ -41,6 +41,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
+        return new BCryptPasswordEncoder();
     }
 }
