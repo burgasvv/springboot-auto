5c58eba773a6cd7479744333920ac98e8a115ee9
Burgas <burgassme@gmail.com>
2024-07-12 18:15:23 +0700

    Добавление стилей

--- src/main/java/com/burgas/springbootauto/repository/person/PersonRepository.java
+++ src/main/java/com/burgas/springbootauto/repository/person/PersonRepository.java
@@ -1,15 +1,12 @@
 package com.burgas.springbootauto.repository.person;
 
 import com.burgas.springbootauto.entity.person.Person;
-import com.burgas.springbootauto.entity.person.Status;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
