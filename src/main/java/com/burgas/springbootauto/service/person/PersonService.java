e2506d55d2fe81b0a1fb0488cfe61da9d9d5e60b
Burgas <burgassme@gmail.com>
2024-07-12 18:14:39 +0700

    Добавление стилей

--- src/main/java/com/burgas/springbootauto/service/person/PersonService.java
+++ src/main/java/com/burgas/springbootauto/service/person/PersonService.java
@@ -56,10 +56,6 @@ public class PersonService {
         return personRepository.findPersonByEmail(email);
     }
 
-    public List<Person> connectedPeople() {
-        return personRepository.findPeopleByStatus(Status.ONLINE);
-    }
-
     @SneakyThrows
     @Transactional
     public Person createUser(Person person, MultipartFile multipartFile) {
