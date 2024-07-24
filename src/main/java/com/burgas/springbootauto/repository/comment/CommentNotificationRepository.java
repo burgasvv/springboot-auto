02496711df6c47f59ab07f8de2c83f61344035f2
Burgas <burgassme@gmail.com>
2024-07-24 13:17:20 +0700

    Добавление ищменений

--- src/main/java/com/burgas/springbootauto/repository/comment/CommentNotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.burgas.springbootauto.repository.comment;
-
-import com.burgas.springbootauto.entity.comment.CommentNotification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentNotificationRepository extends JpaRepository<CommentNotification, Long> {
-}
