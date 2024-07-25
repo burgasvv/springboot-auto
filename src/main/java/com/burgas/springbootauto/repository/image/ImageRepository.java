cb930fbe300c6204e915f018cd482299d2b68cff
Burgas <burgassme@gmail.com>
2024-07-25 17:47:28 +0700

    Установка транзакционности

--- src/main/java/com/burgas/springbootauto/repository/image/ImageRepository.java
+++ src/main/java/com/burgas/springbootauto/repository/image/ImageRepository.java
@@ -1,13 +1,10 @@
 package com.burgas.springbootauto.repository.image;
 
-import com.burgas.springbootauto.entity.car.Car;
 import com.burgas.springbootauto.entity.image.Image;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
