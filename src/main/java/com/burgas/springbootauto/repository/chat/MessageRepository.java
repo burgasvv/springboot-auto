20d2aae1d848910992fffbea06d09647546565b0
Burgas <burgassme@gmail.com>
2024-07-28 18:11:31 +0700

    Добавление дополнительных таблиц и связей

--- src/main/java/com/burgas/springbootauto/repository/chat/MessageRepository.java
+++ src/main/java/com/burgas/springbootauto/repository/chat/MessageRepository.java
@@ -2,7 +2,6 @@ package com.burgas.springbootauto.repository.chat;
 
 import com.burgas.springbootauto.entity.communication.chat.Chat;
 import com.burgas.springbootauto.entity.communication.chat.Message;
-import com.burgas.springbootauto.entity.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,12 +9,6 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-
-    List<Message> findMessagesBySenderAndReceiver(Person sender, Person receiver);
-
-    List<Message> findMessagesBySender(Person sender);
-
-    List<Message> findMessagesByReceiver(Person receiver);
 
     List<Message> findMessagesByChat(Chat chat);
 }
